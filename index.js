7f202f94057e4574c28cfe73d48fa3ca8f3907ed
sarnick005 <sarnick.chakraborty@gmail.com>
2024-03-05 15:37:52 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+ export function scTemperatureConverter(temp, fromTemp, toTemp){
+    fromTemp = fromTemp.toLowerCase();
+    toTemp = toTemp.toLowerCase();
+
+    if ((fromTemp === "c") && (toTemp === "f")) {
+        return (temp * 9 / 5) + 32;
+    } else if ((fromTemp === "f") && (toTemp === "c")) {
+        return (temp - 32) * 5 / 9;
+    } else if ((fromTemp === "f") && (toTemp === "k")) {
+        return ((5 * temp - 160) / 9) + 273.15;
+    } else if ((fromTemp === "k") && (toTemp === "f")) {
+        return ((5 * (temp - 273.15) - 160) / 9);
+    } else if ((fromTemp === "k") && (toTemp === "c")) {
+        return temp - 273.15;
+    } else if ((fromTemp === "c") && (toTemp === "k")) {
+        return temp + 273.15;
+    } else {
+        return "Invalid temperature conversion.";
+    }
+}
